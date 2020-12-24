70ece2c158de15e5d618d2ebe675bac9bced3c72
Ercan Yildiz <ercan_309@hotmail.de>
2020-12-24 17:46:43 +0100

    simplifying link creation with model assembler

--- src/main/java/com/yildiz/Payroll/Employee/EmployeeController.java
+++ src/main/java/com/yildiz/Payroll/Employee/EmployeeController.java
@@ -24,10 +24,12 @@ import org.springframework.web.bind.annotation.RestController;
 public class EmployeeController {
 	
 	private final EmployeeRepository repository;
+	private final EmployeeModelAssembler assembler;
 	
 	//An EmployeeRepository is injected by constructor into the controller.
-	EmployeeController(EmployeeRepository repository){
+	EmployeeController(EmployeeRepository repository, EmployeeModelAssembler assembler){
 		this.repository = repository;
+		this.assembler = assembler;
 	}
 	
 	/*
@@ -42,9 +44,7 @@ public class EmployeeController {
 	CollectionModel<EntityModel<Employee>> all() {
 
 	  List<EntityModel<Employee>> employees = repository.findAll().stream()
-	      .map(employee -> EntityModel.of(employee,
-	          linkTo(methodOn(EmployeeController.class).one(employee.getId())).withSelfRel(),
-	          linkTo(methodOn(EmployeeController.class).all()).withRel("employees")))
+	      .map(assembler::toModel)
 	      .collect(Collectors.toList());
 
 	  return CollectionModel.of(employees, linkTo(methodOn(EmployeeController.class).all()).withSelfRel());
@@ -71,15 +71,7 @@ public class EmployeeController {
 	  Employee employee = repository.findById(id) 
 	      .orElseThrow(() -> new EmployeeNotFoundException(id));
 
-	  /*
-	   * linkTo(methodOn(EmployeeController.class).one(id)).withSelfRel()
-	   *  asks that Spring HATEOAS build a link to the EmployeeController 's 
-	   *  one() method, and flag it as a self link.
-	   */
-	  
-	  return EntityModel.of(employee,
-	      linkTo(methodOn(EmployeeController.class).one(id)).withSelfRel(),
-	      linkTo(methodOn(EmployeeController.class).all()).withRel("employees"));
+	  return assembler.toModel(employee);
 	}
 
 	@PutMapping("/employees/{id}")
---
+++ src/main/java/com/yildiz/Payroll/Employee/EmployeeModelAssembler.java
@@ -0,0 +1,30 @@
+package com.yildiz.Payroll.Employee;
+
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.server.RepresentationModelAssembler;
+import org.springframework.stereotype.Component;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*;
+
+/*
+ * This simple interface has one method: toModel(). It is based on 
+ * converting a non-model object (Employee) into a model-based object 
+ * (EntityModel<Employee>).
+ * 
+ * To leverage this assembler, you only have to alter the EmployeeController 
+ * by injecting the assembler in the constructor.
+ * 
+ */
+
+@Component
+public class EmployeeModelAssembler implements
+RepresentationModelAssembler<Employee, EntityModel<Employee>>{
+
+	@Override
+	public EntityModel<Employee> toModel(Employee employee) {
+	    return EntityModel.of(employee, //
+	            linkTo(methodOn(EmployeeController.class).one(employee.getId())).withSelfRel(),
+	            linkTo(methodOn(EmployeeController.class).all()).withRel("employees"));
+	      }
+	
+
+}
